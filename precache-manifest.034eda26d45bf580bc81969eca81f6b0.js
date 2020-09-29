214a1c3de82043b5ec2b19e07964445374f6d3d5
Evelina Björkman <evelina.v.bjorkman@gmail.com>
2020-09-29 14:36:40 +0200

    Updates

---
+++ precache-manifest.034eda26d45bf580bc81969eca81f6b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58d6cb85ddad02c77da9450b8b960e4",
+    "url": "/js-business-task/index.html"
+  },
+  {
+    "revision": "ed730e7d51ce445c185e",
+    "url": "/js-business-task/static/js/2.2a19efd8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/js-business-task/static/js/2.2a19efd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d3e9cef48b6da89a5cb",
+    "url": "/js-business-task/static/js/main.49e95b43.chunk.js"
+  },
+  {
+    "revision": "caf215d930b51290e785",
+    "url": "/js-business-task/static/js/runtime-main.e80e470b.js"
+  }
+]);
